e12d385cec8443332a6b094674eaeb3d75546ecf
RoLuNeo080521 <romualdbernier36260@gmail.com>
2026-05-25 10:26:06 +0200

    Update NativeFileStreamFactory.cs
    Addition of the insensitive method to the case

--- src/NexusMods.Sdk/IO/NativeFileStreamFactory.cs
+++ src/NexusMods.Sdk/IO/NativeFileStreamFactory.cs
@@ -1,4 +1,46 @@
+using System.Linq;
 using NexusMods.Paths;
+
+namespace NexusMods.Sdk.IO;
+
+/// <summary>
+/// Represents a Stream Factory backed by a native path on the FileSystem.
+/// </summary>
+public class NativeFileStreamFactory : IStreamFactory
+{
+    /// <summary/>
+    /// <param name="file">Absolute path of the file.</param>
+    public NativeFileStreamFactory(AbsolutePath file) => Path = file;
+
+    /// <inheritdoc/>
+    public RelativePath FileName => Path.Name;
+
+    /// <summary>
+    /// Absolute path to the file.
+    /// </summary>
+    public AbsolutePath Path { get; }
+
+    /// <inheritdoc />
+    public ValueTask<Stream> GetStreamAsync()
+    {
+        if (Path.FileExists)
+            return new ValueTask<Stream>(Path.Open(FileMode.Open, FileAccess.Read, FileShare.Read));
+
+        var parent = Path.Parent;
+        if (parent.DirectoryExists())
+        {
+            var fileName = Path.Name;
+            var matches = parent.EnumerateFiles()
+                .Where(f => f.Name.Equals(fileName, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            if (matches.Count > 0)
+                return new ValueTask<Stream>(matches[0].Open(FileMode.Open, FileAccess.Read, FileShare.Read));
+        }
+
+        return new ValueTask<Stream>(Path.Open(FileMode.Open, FileAccess.Read, FileShare.Read));
+    }
+}using NexusMods.Paths;
 
 namespace NexusMods.Sdk.IO;
 
